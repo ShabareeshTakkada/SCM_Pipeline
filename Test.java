861b526b71e3bdb8e4e400d85cd53e6c0c10df44
ShabareeshTakkada <84174237+ShabareeshTakkada@users.noreply.github.com>
2021-07-17 14:20:12 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class TestStringFormatter {  
+public static void main(String[] args) {  
+    System.out.println(StringFormatter.reverseString("my name is khan"));  
+    System.out.println(StringFormatter.reverseString("I am sonoo jaiswal"));      
+    }  
+    
+    public static String reverseString(String str){  
+    char ch[]=str.toCharArray();  
+    String rev="";  
+    for(int i=ch.length-1;i>=0;i--){  
+        rev+=ch[i];  
+    }  
+    return rev;  
+}  
+}  
